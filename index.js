39ad0e8cb157290adb1f66c850e1b05cfd039ae9
Richard Silva <rdsilva_richard@hotmail.com>
2021-03-21 19:50:05 -0300

    fix: path recorder

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const puppeteer = require('puppeteer');
 const PuppeteerScreenRecorder = require('puppeteer-video-recorder');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const recorder = new PuppeteerScreenRecorder();
-  await recorder.init(page); // video must have .mp4 has an extension.
-  await recorder.start(); // video must have .mp4 has an extension.
+  await recorder.init(page, './'); 
+  await recorder.start(); 
   await page.goto('http://www.hillarius.com.br/home-sp/');
 
   //await page.goto('https://www.instagram.com/p/CMF2EiEhSvX/');
